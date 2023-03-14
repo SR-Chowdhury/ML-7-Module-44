665c287cd5528691adebf44f37689285cdbb9948
Shihan Chowdhury <hackedgdown@gmail.com>
2023-03-14 20:01:57 +0000

    44.1 - Six JS fundamentals that you need to know done

---
+++ 44.1-SixFundamentals.js
@@ -0,0 +1,12 @@
+/**
+ * 44.1 - Six JS fundamentals that you need to know
+ */
+
+/**
+ * 1. Decleare variable (const , let)
+ * 2. Conditional operator (if, else if, else, swith case)
+ * 3. Comparision operator (===, !==)
+ * 4. Loop (for of, for in, foreach, while)
+ * 5. Array (declare, lenght, map, find, filter, slice, splice, reduce, inclues)
+ * 6. Object (access property (direct, bracket notaion, bracket notaion with variable))
+ */
